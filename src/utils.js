37fe90ef8470fc7fd50522395a49b7840226dae3
Aftab1311 <md.aftabehsan@gmail.com>
2024-11-06 19:24:29 +0530

    minor change

--- src/utils.js
+++ src/utils.js
@@ -1,43 +1,8 @@
-export const convertImageToBase64 = (file, maxWidth = 800, maxHeight = 800, quality = 0.7) => {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    const reader = new FileReader();
-
-    // Load the file as a data URL
-    reader.readAsDataURL(file);
-    reader.onload = (event) => {
-      img.src = event.target.result;
-    };
-
-    img.onload = () => {
-      // Create a canvas to resize the image
-      const canvas = document.createElement('canvas');
-      let width = img.width;
-      let height = img.height;
-
-      // Maintain aspect ratio and resize if necessary
-      if (width > maxWidth) {
-        height *= maxWidth / width;
-        width = maxWidth;
-      }
-      if (height > maxHeight) {
-        width *= maxHeight / height;
-        height = maxHeight;
-      }
-
-      // Set canvas dimensions
-      canvas.width = width;
-      canvas.height = height;
-      const ctx = canvas.getContext('2d');
-
-      // Draw the image to canvas
-      ctx.drawImage(img, 0, 0, width, height);
-
-      // Convert the canvas to a Base64-encoded JPEG image
-      const base64String = canvas.toDataURL('image/jpeg', quality);
-      resolve(base64String);
-    };
-
-    img.onerror = (error) => reject(error);
-  });
+export const convertImageToBase64 = (file) => {
+  let TYPED_ARRAY = new Uint8Array(file?.data);
+  const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
+    return data + String.fromCharCode(byte);
+  }, "");
+  let base64String = btoa(STRING_CHAR);
+  return `data:image/jpg;base64,${base64String}`;
 };
