e895458d54796b42a4d3109c17d3b10c9baf57c5
Aftab1311 <md.aftabehsan@gmail.com>
2024-11-26 08:45:56 +0530

    few change

--- src/pages/TemporaryDownPage.jsx
+++ src/pages/TemporaryDownPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TemporaryDownPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-red-600 via-red-500 to-red-600 text-white">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-red-600 via-red-500 to-red-600 text-white">
       <div className="text-center px-6 py-10 bg-black bg-opacity-50 rounded-lg shadow-lg">
         <h1 className="text-4xl md:text-6xl font-extrabold mb-4">
           Website Unavailable
